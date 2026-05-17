fead2855c37ac73e2d9022f9a3fdba1151485ea9
Bigblok-ai <haminhtk.1992@gmail.com>
2026-05-17 23:30:36 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,8 +29,8 @@ SOURCES = [
     {"name": "Cakhia247", "url": "https://raw.githubusercontent.com/jasminliu98/cakhia-stream/refs/heads/main/output.json"},
     {"name": "Buncha", "url": "https://raw.githubusercontent.com/xixixius-ai/buncha-stream/refs/heads/main/output.json"},
     {"name": "Giovang", "url": "https://raw.githubusercontent.com/jasminliu98/giovang-stream/refs/heads/main/output.json"},
-    {"name": "Hoiquan", "url": "https://raw.githubusercontent.com/jasminliu98/hoiquan-stream/refs/heads/main/output.json"}
-    {"name": "Quechoa", "url": "https://raw.githubusercontent.com/jasminliu98/quechoa-stream/refs/heads/main/output.json"}
+    {"name": "Hoiquan", "url": "https://raw.githubusercontent.com/jasminliu98/hoiquan-stream/refs/heads/main/output.json"},
+    {"name": "Quechoa", "url": "https://raw.githubusercontent.com/jasminliu98/quechoa-stream/refs/heads/main/output.json"},
     {"name": "Xaycon", "url": "https://raw.githubusercontent.com/jasminliu98/xaycon-stream/refs/heads/main/output.json"}
 ]
 
