05b9f15a7bbd0788201cf41b8153dc2e7a450d0c
Bigblok-ai <haminhtk.1992@gmail.com>
2026-06-07 20:19:40 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ CATEGORIES = {
     "Bóng rổ": "🏀 Bóng Rổ",
     "Billiards": "🎱 Billiards",
     "Bóng chuyền": "🏐 Bóng Chuyền",
-    "Đua xe F1": "🏎️ Đua Xe F1",
+    "Đua xe F1": "🏎️ Đua Xe",
     "Bóng bàn": "🏓 Bóng Bàn",
     "Võ Thuật": "🥊 Võ Thuật",
     "Pickleball": "🏸 Pickleball"
