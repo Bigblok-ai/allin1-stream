19d5967c67c7125650fdcd62c7c9d79a58b6bad2
Bigblok-ai <haminhtk.1992@gmail.com>
2026-09-11 02:08:36 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,7 +29,7 @@ SOURCES = [
     {"name": "Giovang", "url": "https://raw.githubusercontent.com/jasminliu98/giovang-stream/refs/heads/main/output.json"},
     {"name": "Hoiquan", "url": "https://raw.githubusercontent.com/jasminliu98/hoiquan-stream/refs/heads/main/output.json"},
     {"name": "PhaoHoa", "url": "https://raw.githubusercontent.com/jasminliu98/phaohoa-stream/refs/heads/main/output.json"},
-    {"name": "Xaycon", "url": "https://raw.githubusercontent.com/jasminliu98/xaycon-stream/refs/heads/main/output.json"},
+    {"name": "ChuoiChien", "url": "https://raw.githubusercontent.com/jasminliu98/loc-stream/refs/heads/main/output.json"},
     {"name": "ChoangTV", "url": "https://raw.githubusercontent.com/jasminliu98/choang-stream/refs/heads/main/output.json"},
 ]
 
